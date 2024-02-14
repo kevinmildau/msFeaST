2f56f8452ae350500dd0ecfe92c80e030f950d30
Kevin Mildau <kevin.mildau@gmail.com>
2024-02-14 14:08:56 +0100

    fixed coord scaling in js

--- src/dashboardSource.js
+++ src/dashboardSource.js
@@ -94,21 +94,25 @@ function initializeInteractiveNetworkSession(nodes, edges, groups, groupStats) {
     // nodeData: array of objects with x and y numeric data
     // function modified nodes object directly
     if (scalingValue > 0){
-      let xSum = 0;
-      let ySum = 0;
-      let count = 0;
-      for (node of nodes){
-        xSum = xSum + node.x;
-        ySum = ySum + node.y;
-        count = count + 1;
-      }
-      let xMean = xSum / count;
-      let yMean = ySum / count;
+      const xValues = nodes.map(obj => obj.x);
+      const yValues = nodes.map(obj => obj.y);
+      const xMinValue = Math.min(...xValues);
+      const xMaxValue = Math.max(...xValues);
+      const yMinValue = Math.min(...yValues);
+      const yMaxValue = Math.max(...yValues);
+
+      if (xMinValue === xMaxValue){console.error("Error: xmin and xmax coordinates are identical!")}
+      if (yMinValue === yMaxValue){console.error("Error: ymin and ymax coordinates are identical!")}
+
+      const newMax = 1 * scalingValue;
+      const newMin = -1 * scalingValue;
+      const xScaleFactor = (newMax - newMin) / (xMaxValue - xMinValue);
+      const ySaleFactor = (newMax - newMin) / (yMaxValue - yMinValue);
       // Assess current scale of x and y values
       // Set scale to unit scale and multiply with scalingValue
-      for (node of nodes){
-        node.x = (node.x / xMean) * scalingValue; 
-        node.y = (node.y / yMean) * scalingValue;
+      for (let node of nodes){
+        node.x = (node.x - xMinValue) * xScaleFactor + newMin; 
+        node.y = (node.y - yMinValue) * ySaleFactor + newMin;
       }
     }
   }
