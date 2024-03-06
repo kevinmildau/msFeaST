e593fc4a73e7f39e369a1e2994a2eefcab173ca0
Kevin Mildau <kevin.mildau@gmail.com>
2024-03-06 15:47:38 +0100

    moved out interpolation code that wasn't used

--- src/dashboardSource.js
+++ src/dashboardSource.js
@@ -11,23 +11,10 @@ let topkLivePrintContainer = document.getElementById("id-topk-live-print");
 let heatmapContainer = document.getElementById("id_heatmap");
 let runNetworkPhysicsInput = document.getElementById("id-network-physics");
 let colorBarContainer = document.getElementById('id_color_bar');
-let linearInterpolationToPixels = function(value, inputRangeLowerBound, inputRangeUpperBound, outputRangeLowerBound, outputRangeUpperBound){
-  // Take value existing in range of [inputRangeLowerBound, inputRangeUpperBound] and casts it into range of
-  // [outputRangeLowerBound, outputRangeUpperBound]. This assumes the upper and lower bounds are different in both cases.
-  // It is a pure linear interpolation, so no transformation artefacts should be expected.
-  // Based on transforming some variable t in range [a,b] into range [c,d] using formula: f(t) = c + ( ( (d - c) / (b - a) ) * (t - a) )
-  // This function is used to transform statistical measures in a particular range to statistical measures that map well to pixel sizes
-  // Note that output needs to be rounded off to fit into px measures, leading to cutoffs for exponentially or logarithmically behaved variables.
-  // The range between a and b is basically divided equally into pixel steps.
-  out = Math.round(outputRangeLowerBound + ((outputRangeUpperBound - outputRangeLowerBound) / (inputRangeUpperBound - inputRangeLowerBound) * (value - inputRangeLowerBound)))
-  return 
-}
 
 function initializeInteractiveVisualComponents(nodes, edges, groups, groupStats, domElementContrast, domElementMeasure) {
   // Constants
   const colorHighlight = "rgb(235,16,162, 0.9)"// "#EB10A2"
-  //const colorUpRegulated = "#EBB34B"
-  //const colorDownRegulated = "#6EA5EB"
   const defaultEdgeColor = "#A65A8D"
   const defaultNodeColor = "rgb(166, 90, 141, 0.5)" // "#A65A8D"
   const defaultNodeBorderColor = "rgb(0, 0, 0, 0.5)"
