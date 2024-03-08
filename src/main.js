b85c83e0e6dcd71ed2fcf9221d21e5edea09b17b
Kevin Mildau <kevin.mildau@gmail.com>
2024-03-08 17:06:42 +0100

    removed topk and navtabs code

--- src/main.js
+++ src/main.js
@@ -566,24 +566,7 @@ function loadDataAndConstructNetworkVisualization() {
     //fr.onload = receivedText;
     fr.readAsText(file);
   }
-}
-buttonLoadJsonData.addEventListener("click", loadDataAndConstructNetworkVisualization)
+};
+buttonLoadJsonData.addEventListener("click", loadDataAndConstructNetworkVisualization);
 
 
-// Bootstrap component nav tab change listener to force window-resize event upon chaning tabs
-// this is needed for plotly.js and vis.js to properly size the visuals inside the tabs.
-let navTabs = document.querySelector('.nav-pills');
-
-// Add a listener function to the shown.bs.tab event
-navTabs.addEventListener('shown.bs.tab', function (e) {
-  // e.target is the new active tab
-  // e.relatedTarget is the previous active tab
-
-  // Trigger the window resize event
-  window.dispatchEvent(new Event('resize'));
-});
-
-let displaytopkSelected = function (){
-  topkLivePrintContainer.innerHTML = `Selected top-K value: ${this.value}`;
-}
-fromTopkSelector.addEventListener("change", displaytopkSelected);
