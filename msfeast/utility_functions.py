0cb9b310d06725592a9613ab67c0aef7a3fa9922
Kevin Mildau <kevin.mildau@gmail.com>
2024-03-15 15:35:19 +0100

    added utility function

--- msfeast/utility_functions.py
+++ msfeast/utility_functions.py
@@ -1,6 +1,13 @@
 import os
 from warnings import warn
 
+def assert_iloc_valid(iloc : int, iloc_range_max : int) -> None:
+  """ Asserts that a provided iloc is a valid value in range (0, iloc_range_max) """
+  assert iloc in [x for x in range(0, iloc_range_max)], (
+    f"Error: must provide iloc in range of tsne grid 0 to {len(self.tsne_grid)}"
+  )
+  return None
+
 def create_directory_if_not_exists(directory : str) -> None:
   """
   Creates a directory if it does not exist.
