3ab56d5411ab2a45afe18db37299bf68117d8895
Kevin Mildau <kevin.mildau@gmail.com>
2024-03-15 14:15:24 +0100

    created spectral comparison module

--- msfeast/pipeline.py
+++ msfeast/pipeline.py
@@ -15,26 +15,16 @@ from sklearn.metrics import silhouette_score
 
 from grid_entry_classes import GridEntryTsne, GridEntryKmedoid
 from r_output_parsing import _force_to_numeric
+from spectral_comparison import compute_similarities_wrapper, convert_similarity_to_distance
 
 # tsne dependencies
 from sklearn.manifold import TSNE
 from scipy.spatial.distance import pdist, squareform
-from scipy.stats import pearsonr, spearmanr 
-
-# spec2vec dependencies
-#from spec2vec import Spec2Vec
-#import gensim
 
 # plotting functionalities
 import plotly
 import plotly.express
 import plotly.graph_objects as go
-
-# ms2deepscore currently not working with macos m1 processors. Only include if ready to test in windows / linux.
-# ms2deepscore dependencies (does not appear to work for Python 3.10 ; check compatible python version)
-# from ms2deepscore import MS2DeepScore
-# from ms2deepscore.models import load_model
-
 
 
 @dataclass
@@ -260,17 +250,11 @@ class Msfeast:
         "Error: Similarities were already computed or set. "
         "To replace existing scores set Force to True or re-initialize the pipeline."
       )
-    if score_name in ["ms2deepscore", "spec2vec"] and model_directory is None:
-      warn(
-        "Warning: when using ML based scores a model directory with pre-trained models needs to be provided. Not run."
-      )
-    if score_name == "ms2deepscore":
-      similarity_array = _compute_similarities_ms2ds(self.spectra_matchms, model_directory)
-    if score_name == "spec2vec":
-      similarity_array = _compute_similarities_s2v(self.spectra_matchms, model_directory)
-    if score_name == "ModifiedCosine":
-      similarity_array = _compute_similarities_cosine(self.spectra_matchms, cosine_type="ModifiedCosine")
-      # if successful, upade object and attach similarities
+    similarity_array = compute_similarities_wrapper(self.spectra_matchms, score_name, model_directory)
+
+
+
+
     self.similarity_array = similarity_array
     self._attach_settings_used(score_name = score_name)
     return None
@@ -476,7 +460,7 @@ class Msfeast:
     # Subset k_values
     k_values = [value for value in k_values if value < len(self.spectra_matchms)]
     _check_k_values(k_values, len(self.spectra_matchms))
-    distance_matrix = _convert_similarity_to_distance(self.similarity_array)
+    distance_matrix = convert_similarity_to_distance(self.similarity_array)
     self.kmedoid_grid = _run_kmedoid_grid(distance_matrix, k_values)
     _print_kmedoid_grid(self.kmedoid_grid)
     return None
@@ -594,7 +578,7 @@ class Msfeast:
     # Subset perplexity values
     perplexity_values = [perplexity for perplexity in perplexity_values if perplexity < len(self.spectra_matchms)]
     _check_perplexities(perplexity_values, len(self.spectra_matchms))
-    distance_matrix = _convert_similarity_to_distance(self.similarity_array)
+    distance_matrix = convert_similarity_to_distance(self.similarity_array)
     self.tsne_grid = _run_tsne_grid(distance_matrix, perplexity_values)
     _print_tsne_grid(self.tsne_grid)
     return None
@@ -734,115 +718,6 @@ def _load_spectral_data(filepath : str, identifier_key : str = "feature_id") -> 
     assert isinstance(spec, matchms.Spectrum), "Error: all entries in list must be type spectrum."
   return spectra_matchms
 
-def _compute_similarities_ms2ds(
-    spectrum_list:List[matchms.Spectrum], 
-    model_path:str
-    ) -> np.ndarray:
-    """ Function computes pairwise similarity matrix for list of spectra using pretrained ms2deepscore model.
-    
-    Parameters
-        spectrum_list: List of matchms ms/ms spectra. These should be pre-processed and must incldue peaks.
-        model_path: Location of ms2deepscore pretrained model file path (filename ending in .hdf5 or file-directory)
-    Returns: 
-        ndarray with shape (n, n) where n is the number of spectra (Pairwise similarity matrix).
-    """
-    import ms2deepscore # This makes the dependency issue only a problem when this function is called
-    filename = _return_model_filepath(model_path, ".hdf5")
-    model = ms2deepscore.models.load_model(filename) # Load ms2ds model
-    similarity_measure = ms2deepscore.MS2DeepScore(model)
-    scores_matchms = matchms.calculate_scores(
-        spectrum_list, spectrum_list, similarity_measure, is_symmetric=True, array_type="numpy"
-    )
-    scores_ndarray = scores_matchms.to_array()
-    scores_ndarray = np.clip(scores_ndarray, a_min = 0, a_max = 1) # Clip to deal with floating point issues
-    return scores_ndarray
-
-def _compute_similarities_s2v(
-      spectrum_list:List[matchms.Spectrum], 
-      model_path:str,
-      apply_linear_transform : bool = True
-      ) -> np.ndarray:
-    """ Function computes pairwise similarity matrix for list of spectra using pretrained spec2vec model. Uses linear
-    transform to case similarities to range 0 to 1. 
-    
-    Parameters:
-        spectrum_list: List of matchms ms/ms spectra. These should be pre-processed and must incldue peaks.
-        model_path: Location of spec2vec pretrained model file path (filename ending in .model or file-directory)
-        apply_linear_transform : Recasts spec2vec scores from -1 to 1 range to 0 to 1 range.
-    Returns: 
-        ndarray with shape (n, n) where n is the number of spectra (Pairwise similarity matrix).
-    """
-    filename = _return_model_filepath(model_path, ".model")
-    model = gensim.models.Word2Vec.load(filename) # Load s2v model
-    similarity_measure = Spec2Vec(model=model)
-    scores_matchms = matchms.calculate_scores(
-        spectrum_list, spectrum_list, similarity_measure, is_symmetric=True, array_type="numpy")
-    scores_ndarray = scores_matchms.to_array()
-    if apply_linear_transform:
-      scores_ndarray = (scores_ndarray + 1) / 2 # linear scaling from range -1 to 1 to 0 to 1
-    scores_ndarray = np.clip(scores_ndarray, a_min = 0, a_max = 1) # Clip to deal with floating point issues
-    return scores_ndarray
-
-def _compute_similarities_cosine(
-    spectrum_list:List[matchms.Spectrum], 
-    cosine_type : str = "ModifiedCosine"
-    ) -> np.ndarray:
-    """ Function computes pairwise similarity matrix for list of spectra using specified cosine score. 
-    
-    Parameters:
-        spectrum_list: List of matchms ms/ms spectra. These should be pre-processed and must incldue peaks.
-        cosine_type: String identifier of supported cosine metric, options: ["ModifiedCosine", "CosineHungarian", 
-        "CosineGreedy"]
-    Returns:
-        ndarray with shape (n, n) where n is the number of spectra (Pairwise similarity matrix).
-    """
-    valid_types = ["ModifiedCosine", "CosineHungarian", "CosineGreedy"]
-    assert cosine_type in valid_types, f"Cosine type specification invalid. Use one of: {str(valid_types)}"
-    if cosine_type == "ModifiedCosine":
-        similarity_measure = matchms.similarity.ModifiedCosine()
-    elif cosine_type == "CosineHungarian":
-        similarity_measure = matchms.similarity.CosineHungarian()
-    elif cosine_type == "CosineGreedy":
-        similarity_measure = matchms.similarity.CosineGreedy()
-    tmp = matchms.calculate_scores(
-      spectrum_list, spectrum_list, similarity_measure, is_symmetric=True, array_type = "numpy"
-    )
-    scores = _extract_similarity_scores_from_matchms_cosine_array(tmp.to_array())
-    scores = np.clip(scores, a_min = 0, a_max = 1) 
-    return scores
-
-def _extract_similarity_scores_from_matchms_cosine_array(
-    tuple_array : np.ndarray
-    ) -> np.ndarray:
-    """ 
-    Function extracts similarity matrix from matchms cosine scores array.
-    
-    The cosine score similarity output of matchms stores output in a numpy array of pair-tuples, where each tuple 
-    contains (sim, n_frag_overlap). This function extracts the sim scores, and returns a numpy array corresponding to 
-    pairwise similarity matrix.
-
-    Parameters:
-        tuple_array: A single matchms spectrum object.
-    Returns:  
-        A np.ndarray with shape (n, n) where n is the number of spectra deduced from the dimensions of the input
-        array. Each element of the ndarray contains the pairwise similarity value.
-    """
-    sim_data = [ ]
-    for row in tuple_array:
-        for elem in row:
-            sim_data.append(float(elem[0]))
-    return(np.array(sim_data).reshape(tuple_array.shape[0], tuple_array.shape[1]))
-
-def _convert_similarity_to_distance(similarity_matrix : np.ndarray) -> np.ndarray:
-  """ 
-  Converts pairwise similarity matrix to distance matrix with values between 0 and 1. Assumes that the input is a
-  similarity matrix with values in range 0 to 1 up to floating point error.
-  """
-  distance_matrix = 1.- similarity_matrix
-  distance_matrix = np.round(distance_matrix, 6) # Round to deal with floating point issues
-  distance_matrix = np.clip(distance_matrix, a_min = 0, a_max = 1) # Clip to deal with floating point issues
-  return distance_matrix
-
 def _assert_similarity_matrix(scores : np.ndarray, n_spectra : int) -> None:
   """ Function checks whether similarity matrix corresponds to expected formatting. Aborts code if not. """
   assert (isinstance(scores, np.ndarray)), "Error: input scores must be type np.ndarray."
@@ -852,34 +727,7 @@ def _assert_similarity_matrix(scores : np.ndarray, n_spectra : int) -> None:
   assert np.logical_and(scores >= 0, scores <= 1).all(), "Error: all score values must be in range 0 to 1."
   return None
 
-def _return_model_filepath(
-    path : str, 
-    model_suffix:str
-    ) -> str:
-    """ Function parses path input into a model filepath. If a model filepath is provided, it is returned unaltered , if 
-    a directory path is provided, the model filepath is searched for and returned.
 
-    :param path: File path or directory containing model file with provided model_suffix.
-    :param model_suffix: Model file suffix (str)
-    :returns: Filepath (str).
-    :raises: Error if no model in file directory or filepath does not exist. Error if more than one model in directory.
-    """
-    filepath = []
-    if path.endswith(model_suffix):
-        # path provided is a model file, use the provided path
-        filepath = path
-        assert os.path.exists(filepath), "Provided filepath does not exist!"
-    else:
-        # path provided is not a model filepath, search for model file in provided directory
-        for root, _, files in os.walk(path):
-            for file in files:
-                if file.endswith(model_suffix):
-                    filepath.append(os.path.join(root, file))
-        assert len(filepath) > 0, f"No model file found in given path with suffix '{model_suffix}'!"
-        assert len(filepath) == 1, (
-        "More than one possible model file detected in directory! Please provide non-ambiguous model directory or"
-        "filepath!")
-    return filepath[0]
 
 def _run_kmedoid_grid(
     distance_matrix : np.ndarray, 
---
+++ msfeast/spectral_comparison.py
@@ -0,0 +1,175 @@
+import matchms
+from typing import Union, List
+from warnings import warn
+import numpy as np
+import os
+
+# Dev Note: imports for ms2deepscore and spec2vec are currently in calling functions to avoid dependency problems.
+
+def compute_similarities_wrapper(
+  spectra : List[matchms.Spectrum], 
+  score_name : str = "ModifiedCosine",
+  model_directory: Union[str, None] = None
+  ) -> Union[np.ndarray, None]:
+  """ 
+  Functions delegates similarity computations to appropriate score specific function. 
+  """
+  if score_name in ["ms2deepscore", "spec2vec"] and model_directory is None:
+    warn(
+      "Warning: when using ML based scores a model directory with pre-trained models needs to be provided. Not run."
+    )
+    return None
+  if score_name not in ["ms2deepscore", "spec2vec", "ModifiedCosine", "CosineHungarian", "CosineGreedy"]:
+    warn(f"The provided score {score_name} is not supported.")
+    return None
+  if score_name == "ms2deepscore":
+    similarity_array = _compute_similarities_ms2ds(spectra, model_directory)
+  if score_name == "spec2vec":
+    similarity_array = _compute_similarities_s2v(spectra, model_directory)
+  if score_name in ["ModifiedCosine", "CosineHungarian", "CosineGreedy"]:
+    similarity_array = _compute_similarities_cosine(spectra, cosine_type="ModifiedCosine")
+    # if successful, upade object and attach similarities
+  return similarity_array
+
+def convert_similarity_to_distance(similarity_matrix : np.ndarray) -> np.ndarray:
+  """ 
+  Converts pairwise similarity matrix to distance matrix with values between 0 and 1. Assumes that the input is a
+  similarity matrix with values in range 0 to 1 up to floating point error.
+  """
+  distance_matrix = 1.- similarity_matrix
+  distance_matrix = np.round(distance_matrix, 6) # Round to deal with floating point issues
+  distance_matrix = np.clip(distance_matrix, a_min = 0, a_max = 1) # Clip to deal with floating point issues
+  return distance_matrix
+
+def _compute_similarities_cosine(
+    spectrum_list:List[matchms.Spectrum], 
+    cosine_type : str = "ModifiedCosine"
+    ) -> np.ndarray:
+    """ Function computes pairwise similarity matrix for list of spectra using specified cosine score. 
+    
+    Parameters:
+        spectrum_list: List of matchms ms/ms spectra. These should be pre-processed and must incldue peaks.
+        cosine_type: String identifier of supported cosine metric, options: ["ModifiedCosine", "CosineHungarian", 
+        "CosineGreedy"]
+    Returns:
+        ndarray with shape (n, n) where n is the number of spectra (Pairwise similarity matrix).
+    """
+    valid_types = ["ModifiedCosine", "CosineHungarian", "CosineGreedy"]
+    assert cosine_type in valid_types, f"Cosine type specification invalid. Use one of: {str(valid_types)}"
+    if cosine_type == "ModifiedCosine":
+        similarity_measure = matchms.similarity.ModifiedCosine()
+    elif cosine_type == "CosineHungarian":
+        similarity_measure = matchms.similarity.CosineHungarian()
+    elif cosine_type == "CosineGreedy":
+        similarity_measure = matchms.similarity.CosineGreedy()
+    tmp = matchms.calculate_scores(
+      spectrum_list, spectrum_list, similarity_measure, is_symmetric=True, array_type = "numpy"
+    )
+    scores = _extract_similarity_scores_from_matchms_cosine_array(tmp.to_array())
+    scores = np.clip(scores, a_min = 0, a_max = 1) 
+    return scores
+
+def _extract_similarity_scores_from_matchms_cosine_array(
+  tuple_array : np.ndarray
+  ) -> np.ndarray:
+  """ 
+  Function extracts similarity matrix from matchms cosine scores array.
+  
+  The cosine score similarity output of matchms stores output in a numpy array of pair-tuples, where each tuple 
+  contains (sim, n_frag_overlap). This function extracts the sim scores, and returns a numpy array corresponding to 
+  pairwise similarity matrix.
+
+  Parameters:
+      tuple_array: A single matchms spectrum object.
+  Returns:  
+      A np.ndarray with shape (n, n) where n is the number of spectra deduced from the dimensions of the input
+      array. Each element of the ndarray contains the pairwise similarity value.
+  """
+  sim_data = [ ]
+  for row in tuple_array:
+    for elem in row:
+      sim_data.append(float(elem[0]))
+  return(np.array(sim_data).reshape(tuple_array.shape[0], tuple_array.shape[1]))
+
+def _return_model_filepath(
+  path : str, 
+  model_suffix:str
+  ) -> str:
+  """ Function parses path input into a model filepath. If a model filepath is provided, it is returned unaltered , if 
+  a directory path is provided, the model filepath is searched for and returned.
+
+  :param path: File path or directory containing model file with provided model_suffix.
+  :param model_suffix: Model file suffix (str)
+  :returns: Filepath (str).
+  :raises: Error if no model in file directory or filepath does not exist. Error if more than one model in directory.
+  """
+  filepath = []
+  if path.endswith(model_suffix):
+    # path provided is a model file, use the provided path
+    filepath = path
+    assert os.path.exists(filepath), "Provided filepath does not exist!"
+  else:
+    # path provided is not a model filepath, search for model file in provided directory
+    for root, _, files in os.walk(path):
+      for file in files:
+        if file.endswith(model_suffix):
+          filepath.append(os.path.join(root, file))
+    assert len(filepath) > 0, f"No model file found in given path with suffix '{model_suffix}'!"
+    assert len(filepath) == 1, (
+    "More than one possible model file detected in directory! Please provide non-ambiguous model directory or"
+    "filepath!")
+  return filepath[0]
+
+def _compute_similarities_ms2ds(
+  spectrum_list:List[matchms.Spectrum], 
+  model_path:str
+  ) -> np.ndarray:
+  """ Function computes pairwise similarity matrix for list of spectra using pretrained ms2deepscore model.
+  
+  Parameters
+      spectrum_list: List of matchms ms/ms spectra. These should be pre-processed and must incldue peaks.
+      model_path: Location of ms2deepscore pretrained model file path (filename ending in .hdf5 or file-directory)
+  Returns: 
+      ndarray with shape (n, n) where n is the number of spectra (Pairwise similarity matrix).
+  """
+  from ms2deepscore import MS2DeepScore
+  from ms2deepscore.models import load_model  
+  import ms2deepscore # This makes the dependency issue only a problem when this function is called
+  filename = _return_model_filepath(model_path, ".hdf5")
+  model = ms2deepscore.models.load_model(filename) # Load ms2ds model
+  similarity_measure = ms2deepscore.MS2DeepScore(model)
+  scores_matchms = matchms.calculate_scores(
+    spectrum_list, spectrum_list, similarity_measure, is_symmetric=True, array_type="numpy"
+  )
+  scores_ndarray = scores_matchms.to_array()
+  scores_ndarray = np.clip(scores_ndarray, a_min = 0, a_max = 1) # Clip to deal with floating point issues
+  return scores_ndarray
+
+def _compute_similarities_s2v(
+  spectrum_list:List[matchms.Spectrum], 
+  model_path:str,
+  apply_linear_transform : bool = True
+  ) -> np.ndarray:
+  """ Function computes pairwise similarity matrix for list of spectra using pretrained spec2vec model. Uses linear
+  transform to case similarities to range 0 to 1. 
+  
+  Parameters:
+    spectrum_list: List of matchms ms/ms spectra. These should be pre-processed and must incldue peaks.
+    model_path: Location of spec2vec pretrained model file path (filename ending in .model or file-directory)
+    apply_linear_transform : Recasts spec2vec scores from -1 to 1 range to 0 to 1 range.
+  Returns: 
+    ndarray with shape (n, n) where n is the number of spectra (Pairwise similarity matrix).
+  """
+  from spec2vec import Spec2Vec
+  import gensim
+  filename = _return_model_filepath(model_path, ".model")
+  model = gensim.models.Word2Vec.load(filename) # Load s2v model
+  similarity_measure = Spec2Vec(model=model)
+  scores_matchms = matchms.calculate_scores(
+    spectrum_list, spectrum_list, similarity_measure, is_symmetric=True, array_type="numpy"
+  )
+  scores_ndarray = scores_matchms.to_array()
+  if apply_linear_transform:
+    scores_ndarray = (scores_ndarray + 1) / 2 # linear scaling from range -1 to 1 to 0 to 1
+  scores_ndarray = np.clip(scores_ndarray, a_min = 0, a_max = 1) # Clip to deal with floating point issues
+  return scores_ndarray
